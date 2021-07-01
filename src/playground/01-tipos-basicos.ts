59ef1a372353ca3865d0df1345b6ef5123aa9bce
Ricardo Olarte <raop155@hotmail.com>
2021-06-30 23:05:46 -0500

    01 Tipos basicos

---
+++ src/playground/01-tipos-basicos.ts
@@ -0,0 +1,12 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+let nombre: string = "Ricardo"
+let hp: number | string = 95
+let estaVivo: Boolean = true
+
+hp = "FULL"
+
+console.log(nombre, hp)
+
