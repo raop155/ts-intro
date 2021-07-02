47dcfe73b6297b4459cd4c35ac930007b170826a
Ricardo Olarte <raop155@hotmail.com>
2021-07-02 16:16:57 -0500

    Fix index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,3 @@
 /*
     ===== Código de TypeScript =====
 */
-
-interface Pasajero {
-    nombre: string;
-    hijos?: string[];
-}
-
-const pasajero1: Pasajero = {
-    nombre: "Ricardo"
-}
-
-const pasajero2: Pasajero = {
-    nombre: "Fiorella",
-    hijos: ["Ricardo", "Lucciano"]
-}
-
-function imprimeHijos(pasajero: Pasajero): void {
-    const cuantosHijos = pasajero.hijos?.length || 0
-    console.log(cuantosHijos);
-}
-
-imprimeHijos(pasajero1)
